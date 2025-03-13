7526d7eb8c960ecda02cc0f83420d023ed0e9670
cybardev <sheikh@cybar.dev>
2025-03-13 00:19:38 -0300

    re: simplify channel structure

--- default.nix
+++ default.nix
@@ -1,13 +1,19 @@
-let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {
-    overlays = [
-      (self: super: {
-        cutefetch = pkgs.callPackage ./pkgs/cutefetch;
-        freej2me = pkgs.callPackage ./pkgs/freej2me;
-        ytgo = pkgs.callPackage ./pkgs/ytgo;
-      })
-    ];
-  };
-in
-pkgs
+{
+  lib,
+  pkgs ? import <nixpkgs> {
+    config = {
+      allowUnfreePredicate =
+        pkg:
+        builtins.elem (lib.getName pkg) [
+          "freej2me"
+        ];
+    };
+    overlays = [ ];
+  },
+  ...
+}:
+{
+  cutefetch = pkgs.callPackage ./pkgs/cutefetch { };
+  freej2me = pkgs.callPackage ./pkgs/freej2me { };
+  ytgo = pkgs.callPackage ./pkgs/ytgo { };
+}
