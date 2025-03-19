d8e47340ffef5583fac5a5c865d6219013bd8da9
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-19 08:41:30 -0300

    feat: add logseq package

--- default.nix
+++ default.nix
@@ -3,5 +3,6 @@
   cutefetch = pkgs.callPackage ./pkgs/cutefetch { };
   freej2me = pkgs.callPackage ./pkgs/freej2me { };
   jitterbugpair = pkgs.callPackage ./pkgs/jitterbugpair { };
+  logseq = pkgs.callPackage ./pkgs/logseq { };
   ytgo = pkgs.callPackage ./pkgs/ytgo { };
 }
