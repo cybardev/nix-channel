1283e7f6f86ff8d42aa5cd138a2f0e89ca3506e5
cybardev <50134239+cybardev@users.noreply.github.com>
2025-04-13 04:07:25 -0300

    feat: add zen zsh prompt

---
+++ pkgs/zen/package.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  pkgs,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
+
+let
+  author = "cybardev";
+  pname = "zen.zsh";
+  version = "2.0";
+in
+stdenvNoCC.mkDerivation {
+  inherit pname;
+  inherit version;
+
+  src = fetchFromGitHub {
+    owner = author;
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-s/YLFdhCrJjcqvA6HuQtP0ADjBtOqAP+arjpFM2m4oQ=";
+  };
+
+  buildInputs = [ pkgs.zsh ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out"
+    cp prompt_zen_setup "$out/"
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Simple and Peaceful Zsh Prompt";
+    homepage = "https://github.com/${author}/${pname}";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.all;
+  };
+}
