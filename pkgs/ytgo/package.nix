2ed361f20b13b37e12a9bca9dea9cf6060617a75
cybardev <50134239+cybardev@users.noreply.github.com>
2025-06-29 22:47:41 -0300

    ytgo: update to v3.2.0

--- pkgs/ytgo/package.nix
+++ pkgs/ytgo/package.nix
@@ -8,7 +8,7 @@
 let
   author = "cybardev";
   pname = "ytgo";
-  version = "3.1.6";
+  version = "3.2.0";
 in
 buildGoModule {
   inherit pname;
@@ -18,7 +18,7 @@ buildGoModule {
     owner = author;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-pClrvBd7pIzXsIJvMihjwfV2Tor6nAHXLx2ilIIn7TU=";
+    hash = "sha256-kIroOjeWPi6jpW/Vpd8B5batcyOaJn9PqySp9//TCm8=";
   };
 
   nativeBuildInputs = [ pkgs.makeWrapper ];
